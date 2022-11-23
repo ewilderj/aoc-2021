6b3efa6e2a14481f24923ba8514112db793e8979
Edd Wilder-James <edd@me.com>
2022-11-23 14:55:03 -0800

    day1 solved

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,3 +1,27 @@
+use itertools::Itertools;
+
+fn count_increases(a: impl IntoIterator<Item = u32>) -> usize {
+    return a
+        .into_iter()
+        .tuple_windows::<(_, _)>()
+        .filter(|(a, b)| a < b)
+        .count();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let i = include_str!("../input.txt")
+        .lines()
+        .map(|x| x.parse::<u32>().unwrap());
+
+    let r = count_increases(i.clone());
+
+    println!("part1: {}", r);
+
+    let r2 = count_increases(
+        i.clone()
+            .tuple_windows::<(_, _, _)>()
+            .map(|(a, b, c)| a + b + c),
+    );
+
+    println!("part2: {}", r2);
 }
